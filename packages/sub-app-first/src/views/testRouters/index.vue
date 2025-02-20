d85283f698f7ec0bd0b9e19cdd02970ced732e4a
yoguoer <1269148583@qq.com>
2025-02-20 09:28:51 +0800

    perf: ⚡️ 修改文案，避免误解。

--- packages/sub-app-first/src/views/testRouters/index.vue
+++ packages/sub-app-first/src/views/testRouters/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="test-msg">
-    <h1>main-app</h1>
+    <h1>sub-app-first</h1>
     <h3>测试给路由跳转参数</h3>
     <el-input class="input-container" type="textarea" :rows="2" v-model="message" placeholder="请输入跳转参数" />
     <div>
@@ -8,36 +8,34 @@
       <el-button @click="testRouterFirstMessage" type="primary">main 测试通信页</el-button>
       <el-button @click="testRouterSecondHome" type="warning"> appSecond 首页</el-button>
       <el-button @click="testRouterSecondMessage" type="warning">appSecond 测试通信页</el-button>
-     
     </div>
   </div>
 </template>
 
 <script setup>
-import  { microAppRouter } from "v-micro-app-plugin";
+import { microAppRouter } from "v-micro-app-plugin";
 import { ref } from "vue";
 const message = ref("");
 function testRouterFirstHome() {
-  microAppRouter.push({name: 'mainApp', path: '/home'})
+  microAppRouter.push({ name: "mainApp", path: "/home" });
 }
 function testRouterFirstMessage() {
-  microAppRouter.push({name: 'mainApp', path: '/maintestMsg'})
+  microAppRouter.push({ name: "mainApp", path: "/maintestMsg" });
 }
 
 function testRouterSecondHome() {
-  microAppRouter.push({name: 'appSecond', path: '/home'})
+  microAppRouter.push({ name: "appSecond", path: "/home" });
 }
 function testRouterSecondMessage() {
-  microAppRouter.push({name: 'appSecond', path: '/secondtestMsg'})
+  microAppRouter.push({ name: "appSecond", path: "/secondtestMsg" });
 }
- 
 </script>
 
 <style scoped>
 .test-msg {
-  text-align: center;
-  padding-top: 100px;
   height: 90vh;
+  padding-top: 100px;
+  text-align: center;
   .input-container {
     width: 300px;
     margin: 0 auto;
--- packages/sub-app-second/src/views/testRouters/index.vue
+++ packages/sub-app-second/src/views/testRouters/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="test-msg">
-    <h1>main-app</h1>
+    <h1>sub-app-second</h1>
     <h3>测试给路由跳转参数</h3>
     <el-input class="input-container" type="textarea" :rows="2" v-model="message" placeholder="请输入跳转参数" />
     <div>
@@ -8,36 +8,34 @@
       <el-button @click="testRouterFirstMessage" type="primary">main 测试通信页</el-button>
       <el-button @click="testRouterSecondHome" type="warning"> appFirst 首页</el-button>
       <el-button @click="testRouterSecondMessage" type="warning">appFirst 测试通信页</el-button>
-     
     </div>
   </div>
 </template>
 
 <script setup>
-import  { microAppRouter } from "v-micro-app-plugin";
+import { microAppRouter } from "v-micro-app-plugin";
 import { ref } from "vue";
 const message = ref("");
 function testRouterFirstHome() {
-  microAppRouter.push({name: 'mainApp', path: '/home'})
+  microAppRouter.push({ name: "mainApp", path: "/home" });
 }
 function testRouterFirstMessage() {
-  microAppRouter.push({name: 'mainApp', path: '/maintestMsg'})
+  microAppRouter.push({ name: "mainApp", path: "/maintestMsg" });
 }
 
 function testRouterSecondHome() {
-  microAppRouter.push({name: 'appFirst', path: '/home'})
+  microAppRouter.push({ name: "appFirst", path: "/home" });
 }
 function testRouterSecondMessage() {
-  microAppRouter.push({name: 'appFirst', path: '/firsttestMsg'})
+  microAppRouter.push({ name: "appFirst", path: "/firsttestMsg" });
 }
- 
 </script>
 
 <style scoped>
 .test-msg {
-  text-align: center;
-  padding-top: 100px;
   height: 90vh;
+  padding-top: 100px;
+  text-align: center;
   .input-container {
     width: 300px;
     margin: 0 auto;
